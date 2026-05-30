2d714e0eb5191ec2304c74e46dd547b6ab3f7ce8
Abdul Razzak <abdulrazzak74832@gmail.com>
2026-05-30 07:37:08 +0530

    Structure added

---
+++ README.md
@@ -0,0 +1,1 @@
+# maven-user-management-http
---
+++ src/main/java/com/example/usermanagement/App.java
@@ -0,0 +1,11 @@
+package com.example.usermanagement;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Started learning the Maven with user management http ");
+    }
+}
